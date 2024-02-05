427d76bd0f9f2bcf6e64ef5ea6730895b9511f85
Thomas Musial <th896493@dal.ca>
2024-02-05 12:28:13 -0400

    navbar3 on mobile is looking smooth

---
+++ .history/src/components/navbar3/navbar3_20240205105234.jsx
@@ -0,0 +1,271 @@
+import React, { useState,useEffect } from "react";
+import './navbar3.css';
+import { Link } from "react-router-dom";
+import { FaArrowRight } from 'react-icons/fa';
+
+const Navbar3 = () => {
+  const [navButtonClicked, setNavButtonClicked] = useState(false);
+  const [activeMenuItem, setActiveMenuItem] = useState(null);
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isHovered, setIsHovered] = useState(null);
+
+  const [returnClicked, setReturnCLicked] = useState(false)
+
+  const [liHovered, setLiHovered] = useState(null);
+
+  const handleListEnter = (index) => {
+    setLiHovered(index)
+    console.log('mouse entered')
+  }
+
+  const handleListLeave = () => {
+    setLiHovered(null)
+    console.log('mouse exited!?')
+  }
+
+  const subMenuReturn = () => {
+    setReturnCLicked(true);
+    setActiveMenuItem((prevActiveMenuItem) => {
+      if (prevActiveMenuItem !== null) {
+        return null;
+      }
+      return 9;
+    });
+    console.log('menu',activeMenuItem)
+    console.log('set return', returnClicked);
+  };
+  
+
+
+
+  const handleNavClick = () => {
+    setNavButtonClicked(!navButtonClicked);
+    setActiveMenuItem(null); // Reset active menu item when clicking on the main nav button
+  };
+
+  const handleMouseEnter = (index) => {
+   isDesktop ? setIsHovered(index) : null
+    console.log('mouse entered')
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(null)
+  }
+
+  useEffect(()=> {
+    const handleResize = () => {
+        setIsDesktop(window.innerWidth >= 1000)
+        if(isDesktop){
+            setActiveMenuItem(null)
+            setNavButtonClicked(false)
+        }
+    }
+
+    window.addEventListener('resize',handleResize)
+
+    return () => {
+        window.removeEventListener('resize',handleResize)
+    }
+},[])
+
+useEffect(() => {
+    if (returnClicked) {
+      setActiveMenuItem(null);
+      setReturnCLicked(false);
+      console.log('set return', returnClicked);
+    }
+  }, [returnClicked]);
+
+  const handleMenuItemClick = (index) => {
+
+
+    console.log('click')
+    setActiveMenuItem(index);
+    
+
+ 
+   
+  };
+
+
+  const serviceList = () => (
+    <>
+    <ul>
+        <li>Web Dev</li>
+        <li>Copywriting</li>
+        <li>Quantum Realm</li>
+    </ul>
+    </>
+  )
+
+  const aboutList = () => (
+    <>
+    <ul>
+        <li>Our mission</li>
+        <li>About us</li>
+        <li>Quantum Realm</li>
+    </ul>
+    </>
+  )
+
+  const listStyle = (index) =>{
+    const selected = index === liHovered
+
+    return {
+        color: selected ? 'rgb(185, 185, 185)' : 'white',
+        // width:'min-content',
+        // padding:'0',
+        // backgroundColor:'red',
+        // margin:'0'
+    }
+  }
+
+
+
+  const navStyle = {
+    right: navButtonClicked ? '0%' : '-200%',
+    transition: 'right 0.3s ease-in',
+    paddingTop: '4rem',
+    paddingLeft: '2rem',
+  };
+
+  const secondaryNavStyle = (index) => {
+    const selected = activeMenuItem === index && activeMenuItem != null ;
+
+    return {
+  
+        right: returnClicked && activeMenuItem === null? '-200%' :  selected ? '0%' : '-200%',
+        transition: 'right 0.3s ease-in',
+        // paddingTop: '4rem',
+        // paddingLeft: '2rem',
+        // marginRight:'3.8rem',
+        // backgroundColor:'red',
+        // width:'40vw',
+        zIndex:'10',
+        padding:0,
+        width:'15px',
+        // transform:'translateX(-2650%)',
+        paddingTop:'4rem',
+        // paddingLeft:'0.0100110101rem'
+ 
+        // width:'10px',
+        // marginRight:'21.5rem'
+        
+      
+    }
+  }
+
+
+//   const desktopStyle = {
+//     opacity:0
+//   }
+
+  const desktopStyle = (index) => {
+
+
+    const selected = index === isHovered
+
+    return {
+        opacity: selected ? 1 : 0,
+        transition:'opacity 0.3s ease-in'
+    }
+
+  }
+
+  return (
+    <nav className="nav3-container">
+      <Link to='/'>
+        <p className="company-name">Company name</p>
+      </Link>
+      <div className="nav3-logo" onClick={() => handleNavClick()}>
+        <div className="nav-line"/>
+        <div className="nav-line"/>
+        <div className="nav-line"/>
+      </div>
+  
+      <ul className="nav3-contents" style={!isDesktop ? navStyle : null}>
+        <p className="x-button" onClick={() => handleNavClick()}>X</p>
+  
+        {isDesktop ? (
+          <>
+            <div className="desktop-wrapper">
+              <li 
+              onMouseEnter={()=>handleMouseEnter(0)}
+              onMouseLeave={()=>{handleMouseLeave()}}
+              >
+                Services 
+                {/* <FaArrowRight/> */}
+                <ul className={`${!isDesktop ? 'nav3-contents secondary' : 'desktop-submenu'}`} style={!isDesktop ? secondaryNavStyle(0) : desktopStyle(0)}>
+                  <button className="back-button" onClick={() => handleMenuItemClick(0)}>
+                    Back
+                  </button>
+                  <button style={listStyle(0)}
+                  onMouseEnter={()=>handleListEnter(0)}
+                  className="sub-li">Web dev</button>
+                  <li style={listStyle(1)}
+                  className="sub-li">Copywriting</li>
+                  <li style={listStyle(2)}
+                  className="sub-li">Quantum Realm</li>
+                </ul>
+              </li>
+            </div>
+  
+            <div className="desktop-wrapper">
+              <li   onMouseEnter={()=>handleMouseEnter(1)}
+              onMouseLeave={()=>{handleMouseLeave()}}
+              >
+                About 
+                {/* <FaArrowRight/> */}
+                <ul className={`${!isDesktop ? 'nav3-contents secondary' : 'desktop-submenu'}`} style={!isDesktop ? secondaryNavStyle(1) : desktopStyle(1)}>
+                  <button className="back-button" onClick={() => handleMenuItemClick(1)}>
+                    Back
+                  </button>
+                  <li className="sub-li">Our story</li>
+                  <li className="sub-li">Process</li>
+                  <li className="sub-li">Our Team</li>
+                </ul>
+              </li>
+            </div>
+  
+            <div className="desktop-wrapper">
+              <li>Contact</li>
+            </div>
+          </>
+        ) : (
+          <>
+           
+          <div className="main-nav">
+            <ul>
+                <li 
+                onMouseEnter={()=>handleListEnter(0)}
+                onMouseLeave={()=>handleListLeave()}
+                style={listStyle(0)}
+                >
+                    Services
+                </li>
+                <li 
+                onMouseEnter={()=>handleListEnter(1)}
+                onMouseLeave={()=>handleListLeave()}
+                style={listStyle(1)}
+                >
+                    About
+                </li>
+                <li 
+                onMouseEnter={()=>handleListEnter(2)}
+                onMouseLeave={()=>handleListLeave()}
+                style={listStyle(2)}
+                >
+                    Contact
+                </li>
+            </ul>
+          </div>
+
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+  
+};
+
+export default Navbar3;
